3612fb0cca730e8dc5d85151930d09ecbfc97c46
aldana <aldanamilesse@gmail.com>
2021-10-27 17:52:37 -0300

    finished project js

--- app.js
+++ app.js
@@ -5,48 +5,93 @@ function computerPlay (){
    return options[posicion];
 }
 
+let contadorComputer = 0;
+let contadorPlayer = 0;
+
 function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() == "rock"){
-        if(computerSelection == "Scissors" ){
-            return "You win! Rock beat Scissors";
-        }
-        else if (computerSelection == "Paper"){
-            return "You Lose! Paper beats Rock"
-        }
-        else {
-            return "empate"
-        }
+    if (contadorComputer < 5 && contadorPlayer < 5){
+        if (playerSelection == "rock"){
+            if(computerSelection == "Scissors" ){
+                    contadorPlayer += 1;
+                    pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+                    return "You win! Rock beat Scissors";
+                }
+            else if (computerSelection == "Paper"){
+                    contadorComputer +=1;
+                    pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+                    return "You Lose! Paper beats Rock"
+                }
+            else {
+                    return "empate"
+                }
+            }
+        else if (playerSelection == "paper"){
+                if (computerSelection == "Scissors" ){
+                    contadorComputer +=1;
+                    pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+                    return "You Loser! Scissors beats Paper"
+                }
+            else if (computerSelection == "Rock"){
+                    contadorPlayer += 1;
+                    pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+                    return "You win! Paper beats Rock"
+                }
+            else {
+                    return "empate"
+                }
+            }
+        else if (playerSelection == "scissors"){
+            if(computerSelection == "Paper"){
+                    contadorPlayer += 1;
+                    pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+                    return "You win! Scissors beats Paper"
+                }
+            else if (computerSelection == "Rock"){
+                    contadorComputer +=1;
+                    pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+                    return "You Loser! Rock beats Scissors"
+                }
+            else {
+                    return "empate"
+                }
+            }
     }
-    else if (playerSelection.toLowerCase() == "paper"){
-        if (computerSelection == "Scissors" ){
-            return "You Loser! Scissors beats Paper"
-        }
-        else if (computerSelection == "Rock"){
-            return "You win! Paper beats Rock"
-        }
-        else {
-            return "empate"
-        }
+    else if (contadorPlayer >= 5){
+        resultadoFinal.textContent = "has ganado";
     }
-    else if (playerSelection.toLowerCase() == "scissors"){
-        if(computerSelection == "Paper"){
-            return "You win! Scissors beats Paper"
-        }
-        else if (computerSelection == "Rock"){
-            return "You Loser! Rock beats Scissors"
-        }
-        else {
-            return "empate"
-        }
+    else if (contadorComputer >= 5){
+        resultadoFinal.textContent = "loser";
     }
+    
 }
 
-function game(){
-    for (let i = 0; i < 5; i++){
-        const playerSelection = prompt("choose between rock, paper or scissors: ");
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+const result = document.querySelector("#result"); 
+const p = document.createElement("p");
+result.appendChild(p);
 
-game()
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+     button.addEventListener("click", () => {
+         let playerSelection = button.id;
+         const computerSelection = computerPlay();
+         p.textContent = (playRound(playerSelection, computerSelection));
+    });
+});
+
+const divContador = document.createElement("div");
+const pContadorComputer = document.createElement("p");
+pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+const pContadorPlayer = document.createElement("p");
+pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+
+const resultadoFinal = document.createElement("p");
+
+
+divContador.appendChild(pContadorComputer);
+divContador.appendChild(pContadorPlayer);
+divContador.appendChild(resultadoFinal);
+
+const container = document.querySelector("#container"); 
+container.appendChild(divContador);
+
+
