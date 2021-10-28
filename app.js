10bd29f9cfdfed6b92b7cf2bf25e9267ef85a4f9
aldana <aldanamilesse@gmail.com>
2021-10-28 10:22:09 -0300

    js project finished

--- app.js
+++ app.js
@@ -13,54 +13,54 @@ function playRound(playerSelection, computerSelection){
         if (playerSelection == "rock"){
             if(computerSelection == "Scissors" ){
                     contadorPlayer += 1;
-                    pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+                    pContadorPlayer.textContent = "Score: " + contadorPlayer;
                     return "You win! Rock beat Scissors";
                 }
             else if (computerSelection == "Paper"){
                     contadorComputer +=1;
-                    pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+                    pContadorComputer.textContent = "Score: "+ contadorComputer;
                     return "You Lose! Paper beats Rock"
                 }
             else {
-                    return "empate"
+                    return "It's a tie"
                 }
             }
         else if (playerSelection == "paper"){
                 if (computerSelection == "Scissors" ){
                     contadorComputer +=1;
-                    pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+                    pContadorComputer.textContent = "Score: "+ contadorComputer;
                     return "You Loser! Scissors beats Paper"
                 }
             else if (computerSelection == "Rock"){
                     contadorPlayer += 1;
-                    pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+                    pContadorPlayer.textContent = "Score: " + contadorPlayer;
                     return "You win! Paper beats Rock"
                 }
             else {
-                    return "empate"
+                    return "It's a tie"
                 }
             }
         else if (playerSelection == "scissors"){
             if(computerSelection == "Paper"){
                     contadorPlayer += 1;
-                    pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+                    pContadorPlayer.textContent = "Score: " + contadorPlayer;
                     return "You win! Scissors beats Paper"
                 }
             else if (computerSelection == "Rock"){
                     contadorComputer +=1;
-                    pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+                    pContadorComputer.textContent = "Score: "+ contadorComputer;
                     return "You Loser! Rock beats Scissors"
                 }
             else {
-                    return "empate"
+                    return "It's a tie"
                 }
             }
     }
     else if (contadorPlayer >= 5){
-        resultadoFinal.textContent = "has ganado";
+        resultadoFinal.textContent = "Game Over. You Win!";
     }
     else if (contadorComputer >= 5){
-        resultadoFinal.textContent = "loser";
+        resultadoFinal.textContent = "Game Over. You Lose!";
     }
     
 }
@@ -68,6 +68,10 @@ function playRound(playerSelection, computerSelection){
 const result = document.querySelector("#result"); 
 const p = document.createElement("p");
 result.appendChild(p);
+
+const resultadoFinal = document.createElement("p");
+result.appendChild(resultadoFinal);
+
 
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => {
@@ -79,19 +83,28 @@ buttons.forEach((button) => {
 });
 
 const divContador = document.createElement("div");
+
+const imagen = document.createElement("img");
+imagen.src = "computerpng.png";
+imagen.classList.add("imagen");
+divContador.appendChild(imagen);
+
 const pContadorComputer = document.createElement("p");
-pContadorComputer.textContent = "contador computer: "+ contadorComputer;
+pContadorComputer.textContent = "Score: "+ contadorComputer;
+divContador.classList.add("contadorComputer");
+pContadorComputer.classList.add("contadorComputerP");
+
 const pContadorPlayer = document.createElement("p");
-pContadorPlayer.textContent = "contador player: " + contadorPlayer;
+pContadorPlayer.classList.add("contadorPlayer");
+pContadorPlayer.textContent = "Score: " + contadorPlayer;
 
-const resultadoFinal = document.createElement("p");
-
+const player = document.querySelector("#player");
+player.appendChild(pContadorPlayer);
 
 divContador.appendChild(pContadorComputer);
-divContador.appendChild(pContadorPlayer);
-divContador.appendChild(resultadoFinal);
 
 const container = document.querySelector("#container"); 
 container.appendChild(divContador);
+
 
 
